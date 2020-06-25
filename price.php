aab1d42801d0f36bdb6cfbd2aec5daa906a512bd
blackestwhite <pesaregoal@gmail.com>
2020-06-25 23:10:50 +0430

    gets prices from binance instead of coingecko

--- price.php
+++ price.php
@@ -1,7 +1,28 @@
 <?php
 header("content-type: application/json");
-$url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cripple%2Ccardano%2Cethereum%2Clitecoin%2Cbitcoin-cash&vs_currencies=usd';
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-$res = curl_exec($ch);
+$btc = json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=BTCUSDT"))->price;
+$eth = json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=ETHUSDT"))->price;
+$xrp = json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=XRPUSDT"))->price;
+$ltc = json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=LTCUSDT"))->price;
+$bch = json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=BCHUSDT"))->price;
+$ada = json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=ADAUSDT"))->price;
+
+$btc = sprintf("%.4f", $btc);
+$eth = sprintf("%.4f", $eth);
+$xrp = sprintf("%.4f", $xrp);
+$ltc = sprintf("%.4f", $ltc);
+$bch = sprintf("%.4f", $bch);
+$ada = sprintf("%.4f", $ada);
+
+$prices = [
+        'btc' => $btc,
+        'eth' => $eth,
+        'xrp' => $xrp,
+        'ltc' => $ltc,
+        'bch' => $bch,
+        'ada' => $ada,
+    ];
+$prices = json_encode($prices);
+echo $prices;
+
 ?>

e8fed95625d2615f77da5e6e886f5acaf401c9b0
blackestwhite <pesaregoal@gmail.com>
2020-11-12 03:03:39 +0330

    added more flexiblity

--- cron.php
+++ cron.php
@@ -1,6 +1,6 @@
 <?php
 include 'Telegram.php';
-include 'config.php';
+include './config.php';
 include 'currencies.php';
 $telegram          = new Telegram($TOKEN);
 $chat_id           = $telegram->ChatID();
@@ -11,70 +11,48 @@ $callback_chat_id  = $telegram->Callback_ChatID();
 $message_id        = $telegram->MessageID();
 
 if ($callback_data == 'now') {
-    $answerText = json_decode( file_get_contents($price_path) );
-    $btc = $answerText->btc;
-    $eth = $answerText->eth;
-    $xrp = $answerText->xrp;
-    $link = $answerText->link;
-    $bch = $answerText->bch;
-    $bnb = $answerText->bnb;
-
-$answerText = "⚡ Prices Right Now :
-
-BTC: \$${btc}
-ETH: \$${eth}
-XRP: \$${xrp}
-LINK: \$${link}
-BCH: \$${bch}
-BNB: \$${bnb}
-";
-    $content = array('callback_query_id' => $callback_query["id"], 'text' => $answerText, 'show_alert' => true);
+    $answerText = json_decode( file_get_contents($GLOBALS['price_path']) );
+    foreach($currencies as $currency){
+        $$currency = $answerText->{"${currency}"};
+    }
+    
+    $answerTxt = "⚡ Prices right now\n";
+    foreach($currencies as $currency){
+        $answerTxt = $answerTxt."\n".strtoupper($currency)." \$".$$currency;
+    }
+    
+    $content = array('callback_query_id' => $callback_query["id"], 'text' => $answerTxt, 'show_alert' => true);
     $telegram->answerCallbackQuery($content);
 }
 
 // pass argument to examine that the request is sent by cron curl client
 if( isset($_GET['do']) ){
-    if($_GET['do'] == 'passed argument') {
+    if($_GET['do'] == 411) {
         function do1() {
             $telegram = $GLOBALS["telegram"];
-            $res = file_get_contents($GLOBALS['price_path']);
+            $res = json_decode(file_get_contents($GLOBALS['price_path']));
+            
             // reads last.txt if exists to calculate price changes
             if (file_exists('last.txt')) {
                 $last = file_get_contents('last.txt');
                 $last = json_decode($last);
             }
-            $res = json_decode($res);
-            //get percentage of price changes
-            $BTCper = floatval($res->btc/$last->btc)*100 - 100;
-            $ETHper = floatval($res->eth/$last->eth)*100 - 100;
-            $XRPper = floatval($res->xrp/$last->xrp)*100 - 100;
-            $LINKper = floatval($res->link/$last->link)*100 - 100;
-            $BCHper = floatval($res->bch/$last->bch)*100 - 100;
-            $BNBper = floatval($res->bnb/$last->bnb)*100 - 100;
-
-            //show 3 decimal points
-        	$BTCper = strval(sprintf("%.3f", $BTCper));
-        	$ETHper = strval(sprintf("%.3f", $ETHper));
-        	$XRPper = strval(sprintf("%.3f", $XRPper));
-        	$LINKper = strval(sprintf("%.3f", $LINKper));
-        	$BCHper = strval(sprintf("%.3f", $BCHper));
-        	$BNBper = strval(sprintf("%.3f", $BNBper));
-        
-                $texttt =
-                    ($last->btc > $res->btc ? "🔴":"🔵").
-                    " BTC:  $".$res->btc."  (*". $BTCper ."%*) \n".
-                    ($last->eth > $res->eth ? "🔴":"🔵").
-                    " ETH:  $".$res->eth."  (*". $ETHper ."%*) \n".
-                    ($last->xrp > $res->xrp ? "🔴":"🔵").
-                    " XRP:  $".$res->xrp."  (*". $XRPper ."%*) \n".
-                    ($last->ltc > $res->ltc ? "🔴":"🔵").
-                    " LINK:  $".$res->link."  (*". $LINKper ."%*) \n".
-                    ($last->bch > $res->bch ? "🔴":"🔵").
-                    " BCH:  $".$res->bch."  (*". $BCHper ."%*) \n".
-                    ($last->ada > $res->ada ?  "🔴":"🔵").
-                    " BNB: $".$res->bnb."  (*". $BNBper ."%*) \n".
-                    "\n @ZarrrinCryptoPrice";
-        
+            
+            foreach($GLOBALS['currencies'] as $cur) {
+                $$cur = floatval($res->{"${cur}"}/$last->{"${cur}"})*100 - 100;
+                $$cur = strval(sprintf("%.3f", $$cur));
+            }
+                
+                $blueBall = "🔵";
+                $redBall = "🔴";
+                $textTxt = "";
+                foreach($GLOBALS['currencies'] as $cur) {
+                    $upperCaseSymbol = strtoupper($cur);
+                    $textTxt = $textTxt.($last->{"${cur}"} > $res->{"${cur}"} ? $redBall : $blueBall)." ${upperCaseSymbol}: $".$res->{"${cur}"}."  (*".$$cur."%*) \n";
+                }
+                $CH_USERNAME = $GLOBALS['CH_USERNAME'];
+                $textTxt = $textTxt."\n${CH_USERNAME}";
+                
             // writes to last.txt the prices to calculate price changes
             $res = json_encode($res);
             $last = file_put_contents('last.txt', $res);
@@ -82,24 +60,22 @@ if( isset($_GET['do']) ){
             
             $option = array( 
         
-        		array(
+                        array(
        
-        			$telegram->buildInlineKeyboardButton("Live Price","","now","")
+                                $telegram->buildInlineKeyboardButton("Live Price","","now","")
         
-        		)
+                        )
         
-        	);
+                );
         
-        	$keyb = $telegram->buildInlineKeyBoard($option);
-            $CH_USERNAME = $GLOBALS["CH_USERNAME"];
-        	$content = array('chat_id' =>  $CH_USERNAME, 'text' => $texttt ,'parse_mode' => 'markdown' ,'reply_markup' => $keyb);
+                $keyb = $telegram->buildInlineKeyBoard($option);
+                $CH_USERNAME = $GLOBALS['CH_USERNAME'];
+                $content = array('chat_id' => $CH_USERNAME, 'text' => $textTxt ,'parse_mode' => 'markdown' ,'reply_markup' => $keyb);
         
-        	$telegram->sendMessage($content);
+                $telegram->sendMessage($content);
         }
         $text = do1();
         
     }
 }
-
-
 ?>

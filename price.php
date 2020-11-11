03b8f04cddb6e04f594b69f4ebd73ce3af287994
blackestwhite <pesaregoal@gmail.com>
2020-11-12 03:07:08 +0330

    uppercase problem in fetching prices

--- price.php
+++ price.php
@@ -2,7 +2,8 @@
 header("content-type: application/json");
 include 'currencies.php';
 foreach ($currencies as $currency){
-  $$currency = sprintf("%.4f",json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=${currency}USDT"))->price);
+  $uppercaseSym = strtoupper($currency);
+  $$currency = sprintf("%.4f",json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=${$uppercaseSym}USDT"))->price);
 }
 $prices = [];
 foreach ($currencies as $cur){

88a2deb1c7f96993d4ca8a74c16804faf421da41
BlackestWhite <pesaregoal@gmail.com>
2021-08-20 18:52:28 +0430

    fix: fix url problem

--- price.php
+++ price.php
@@ -3,7 +3,9 @@ header("content-type: application/json");
 include 'currencies.php';
 foreach ($currencies as $currency){
   $uppercaseSym = strtoupper($currency);
-  $$currency = sprintf("%.4f",json_decode(file_get_contents("https://www.binance.com/api/v3/avgPrice?symbol=${$uppercaseSym}USDT"))->price);
+  $url = "https://www.binance.com/api/v3/avgPrice?symbol=".$uppercaseSym."USDT";
+  $x = json_decode(file_get_contents($url));
+  $$currency = sprintf("%.4f", $x->{'price'});
 }
 $prices = [];
 foreach ($currencies as $cur){

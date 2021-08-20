e9aa49be665a02d80227054701028de33d0b565f
BlackestWhite <pesaregoal@gmail.com>
2021-08-20 18:52:50 +0430

    fix: change logic in if statement

--- currencies.php
+++ currencies.php
@@ -1,11 +1,13 @@
 <?php
-    $url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=11&page=1&sparkline=false';
+    $url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=1&sparkline=false';
     $json = file_get_contents($url);
     $data = json_decode($json, true);
     $currencies = [];
     foreach ($data as $key => $value) {
         // push to array
-        if ($value['symbol'] != 'usdt' || $value['symbol'] != 'usdc') {
+        if ($value['symbol'] == 'usdt' || $value['symbol'] == 'usdc') {
+            continue;
+        }else{
             array_push($currencies, $value['symbol']);
         }
     }
